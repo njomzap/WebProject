6b10f83136fe3cea94cc84a7fca26c44a8c30fbe
eraazeqaj <ez68757@ubt-uni.net>
2024-03-24 23:24:32 +0100

    updated footer with some changes

--- footer.php
+++ footer.php
@@ -1,1 +1,11 @@
-<footer>Footer text </footer>
+<?php if ( ! isset( $_SESSION['loggedin'] ) ) : ?>
+<div class="admin-login">
+    <p><strong>Admin login</strong></p>
+    <p>Email: admin@gmail.com</p>
+    <p>Password: admin</p>
+</div>
+<?php endif ?>
+<footer>
+    <p><?php echo $settings['footer_text']; ?></p>
+    <script src="script.js"></script>
+</footer>
